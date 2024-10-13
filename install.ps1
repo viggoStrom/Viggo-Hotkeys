f8b18a04e72ef38cb2e53f66e26aeb0bfde31975
viggoStrom <viggostrom05@gmail.com>
2024-10-13 17:00:12 +0200

    redid query ahk2

--- install.ps1
+++ install.ps1
@@ -4,15 +4,19 @@ Set-Location "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
 
 # Remove the old AHK file
 # Prompt the user for confirmation before deleting .ahk and .ahk2 files
-$confirmation = Read-Host "Do you want to delete all .ahk and .ahk2 files in the directory? (Y/N)"
-if ($confirmation -eq 'Y') {
-    Remove-Item *.ahk, *.ahk2 -Force
-    Write-Host "Files deleted."
-} else {
-    Write-Host "Deletion cancelled."
-    
-    Write-Host "Exiting script."
-    exit
+# Check if the folder contains .ahk or .ahk2 files
+$ahkFiles = Get-ChildItem -Filter *.ahk
+$ahk2Files = Get-ChildItem -Filter *.ahk2
+if ($ahkFiles -or $ahk2Files) {
+    $confirmation = Read-Host "The folder contains .ahk or .ahk2 files. Do you want to delete them? (Y/N)"
+    if ($confirmation -eq 'Y') {
+        Remove-Item *.ahk, *.ahk2 -Force
+        Write-Host "Files deleted."
+    } else {
+        Write-Host "Deletion cancelled."
+        Write-Host "Exiting script."
+        exit
+    }
 }
 
 # Download all the seperate AHK files
--- readme.md
+++ readme.md
@@ -1,13 +1,21 @@
 # My Hotkeys
 
 ## Description
-I am a real power user, sometimes the OS doesn't provide everything I need, so I have to create my own hotkeys. I have used [AutoHotkey](https://www.autohotkey.com/).
+I am a real power user, sometimes the OS doesn't provide everything I need, so I have to create my own hotkeys. I have used [AutoHotkey](https://www.autohotkey.com/) for mine.
 
 ## Installation
+1. Have [AutoHotkey V1](https://www.autohotkey.com/) and [AutoHotkey V2](https://www.autohotkey.com/) installed.
+
+2. Run the following command in PowerShell:
 ```bash
 wget https://raw.githubusercontent.com/viggoStrom/Viggo-Hotkeys/refs/heads/main/install.ps1
 ./install.ps1
 ```
 
 ## Hotkeys
-- 
+- `Win + C`: Open Web Browser (Firefox as default)
+- `Win + Z`: Open Visual Studio Code
+- `Ctrl + Q`: Web search selected text (DuckDuckGo as default)
+- `F3`: Browser back (Alt to ignore)
+- `F4`: Browser forward (Alt to ignore)
+- `Win + Space`: Non-breaking space
